b501d5905c318f1b5929f5746ae69bdd42abad52
DenisZelenskiy <den.zelenskiy@gmail.com>
2019-09-08 16:55:19 +0300

    Updates

---
+++ precache-manifest.69513bca82d44a51a1a63fc4fb27e144.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d92f35f1efebc38103b8",
+    "url": "/goit-react-hw-02/static/js/runtime~main.577bbb3b.js"
+  },
+  {
+    "revision": "9a7596dff4cd1535388b",
+    "url": "/goit-react-hw-02/static/js/main.0861cdc6.chunk.js"
+  },
+  {
+    "revision": "0ae9b33cb6e81c596ed5",
+    "url": "/goit-react-hw-02/static/js/2.ed4a16b7.chunk.js"
+  },
+  {
+    "revision": "9a7596dff4cd1535388b",
+    "url": "/goit-react-hw-02/static/css/main.5e20fb78.chunk.css"
+  },
+  {
+    "revision": "d899ce31c3dad87b9322c1f0490e324f",
+    "url": "/goit-react-hw-02/index.html"
+  }
+];
